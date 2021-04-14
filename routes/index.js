a8f7e382b2b2a38ce81077da9755d47404b64ba9
edwinz1 <edwinzacatelco1998@gmail.com>
2021-04-14 08:10:40 -0500

    Agregando Nombre de Compania

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express',author: 'Edwin Zacatelco' ,appName: 'WebApp'});
+  res.render('index', { title: 'Express',author: 'Edwin Zacatelco' ,appName: 
+  'WebApp', company: 'Awsome Software'});
 });
 
 router.get('/greeting',function(req,res,next){
