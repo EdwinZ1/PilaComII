fd8e4164cd34d512944d1b6b53bdbb1e6987a586
edwinz1 <edwinzacatelco1998@gmail.com>
2021-06-24 15:25:32 -0500

    sesion 34

--- server/controllers/homeControllers.js
+++ server/controllers/homeControllers.js
@@ -1,12 +1,21 @@
+/* eslint-disable prettier/prettier */
 const index = (req, res) => {
-  res.render('index', {
+  res.render('home/index', {
     title: 'ProjNotes',
   });
 };
 const greeting = (req, res) => {
-  res.status(200).json({ message: 'Hola Campeon de la FullStack web' });
+  res.status(200).json({
+    message: 'Hola Campeon de la FullStack web',
+   });
+};
+const about = (req, res) => {
+  res.render('home/about', {
+    title: 'Acerca de Projnote',
+  });
 };
 export default {
   index,
   greeting,
+  about,
 };
--- server/routes/home.js
+++ server/routes/home.js
@@ -6,10 +6,14 @@ import homeController from '@server/controllers/homeControllers';
 // Creando la instancia de un router
 const router = new Router();
 // GET '/'
-router.get('/', homeController.index);
+router.get(['/', '/index'], homeController.index);
+// router.get('/index', homeController.index);
 
 // Get '/greeeting'
-router.get('/greeting', homeController.greeting)
+router.get('/greeting', homeController.greeting);
+
+//
+router.get('/about', homeController.about);
 // Exportando el router que maneja las subrutas
 // Para el controlador Home
 export default router;
